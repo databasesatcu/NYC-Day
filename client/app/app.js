e7f8dec0fb2a8f88d9b305b823bb42b2feb0767a
Herbert Freyre <herbertfj@gmail.com>
2015-03-30 18:49:36 -0400

    Push of initial files

---
+++ client/app/app.js
@@ -0,0 +1,16 @@
+'use strict';
+
+angular.module('nycDayApp', [
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute'
+])
+  .config(function ($routeProvider, $locationProvider) {
+    $routeProvider
+      .otherwise({
+        redirectTo: '/'
+      });
+
+    $locationProvider.html5Mode(true);
+  });
