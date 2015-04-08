5cc633144c5537d6d8e08f8c8c00caa41dd8383f
Herbert Freyre <herbertfj@gmail.com>
2015-04-08 19:54:14 -0400

    Search updates

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -1,7 +1,12 @@
 'use strict';
 
 angular.module('nycDayApp')
-  .controller('SearchCtrl', function ($scope) {
+  .controller('SearchCtrl', function ($scope, $http) {
     $scope.userEmail = '';
+    $scope.userNeighborhood = '';
 
+    $http.get('/api/data/neighborhood')
+      .success(function(data) {
+        $scope.neighborhoods = data;
+      });
   });
