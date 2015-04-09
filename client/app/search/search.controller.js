10047c2ba35a1d761e2aee7341dcb6cda9bc9edb
Herbert Freyre <herbertfj@gmail.com>
2015-04-08 22:16:52 -0400

    Search data update

--- client/app/search/search.controller.js
+++ client/app/search/search.controller.js
@@ -5,15 +5,18 @@ angular.module('nycDayApp')
     $scope.showForm = true;
     $scope.showResult = false;
 
-    $scope.user = {};
+    var instantiate = function() {
+      $scope.user = {};
+      $scope.user.email = 'herbert@gmail.com';
+      $scope.user.neighborhood = 'GREENWICH VILLAGE';
+      $scope.user.priceRange = 10;
+      $scope.user.cuisine = 'MEXICAN';
+      $scope.user.start = '09:00';
+      $scope.user.end = '20:00';
+      $scope.user.gallery = 'GOYA';
+    };
 
-    $scope.user.email = 'herbert@gmail.com';
-    $scope.user.neighborhood = 'GREENWICH VILLAGE';
-    $scope.user.priceRange = 10;
-    $scope.user.cuisine = 'MEXICAN';
-    $scope.user.start = '09:00';
-    $scope.user.end = '20:00';
-    $scope.user.gallery = 'GOYA';
+    instantiate();
 
     $http.get('/api/data/neighborhood')
       .success(function(data) {
@@ -31,16 +34,84 @@ angular.module('nycDayApp')
       });
 
     $scope.search = function () {
-      $http.post('api/data/searchmuseum', {
+
+      var searchObject = {
         neighborhood : $scope.user.neighborhood,
         cuisine      : $scope.user.cuisine,
         gallery      : $scope.user.gallery
-      }).success(function(data) {
+      };
 
+      $scope.result = {};
+
+      // Food truck
+      $http.post('/api/data/searchfoodtruckcuisine', searchObject).success(function(data) {
+          $scope.result.foodtruck = {};
+          if (data.length) {
+            $scope.result.foodtruck.name = data[0].truck_name;
+            $scope.result.foodtruck.address = data[0].trajectory;
+            $scope.result.foodtruck.cuisine = data[0].cuisine;
+          } else {
+            console.log('first query failed.');
+            $http.post('/api/data/searchfoodtruck', searchObject).success(function (data1) {
+              $scope.result.foodtruck.name = data1[0].truck_name;
+              $scope.result.foodtruck.address = data1[0].trajectory;
+              $scope.result.foodtruck.cuisine = data1[0].cuisine;
+            });
+          }
         });
+
+      // Restaurant
+      $http.post('/api/data/searchrestaurantcuisine', searchObject).success(function(data) {
+        $scope.result.restaurant = {};
+        if (data.length) {
+          $scope.result.restaurant.name = data[0].restaurant_name;
+          $scope.result.restaurant.address = data[0].address;
+          $scope.result.restaurant.cuisine = data[0].cuisine;
+        } else {
+          console.log('first query failed.');
+          $http.post('/api/data/searchrestaurant', searchObject).success(function (data1) {
+            $scope.result.restaurant.name = data1[0].restaurant_name;
+            $scope.result.restaurant.address = data1[0].address;
+            $scope.result.restaurant.cuisine = data1[0].cuisine;
+          });
+        }
+      });
+
+      // Museum
+      $http.post('/api/data/searchmuseumgallery', searchObject).success(function(data) {
+        $scope.result.museum = {};
+        var museumName = '';
+        if (data.length) {
+          $scope.result.museum.name = data[0].museum_name;
+          museumName = data[0].museum_name;
+          $scope.result.museum.style = data[0].style;
+          $scope.result.museum.gallery = data[0].gallery_name;
+        } else {
+          console.log('first query failed.');
+          $http.post('/api/data/searchmuseum', searchObject).success(function (data1) {
+            $scope.result.museum.name = data1[0].museum_name;
+            museumName = data1[0].museum_name;
+            $scope.result.museum.style = data1[0].style;
+            $scope.result.museum.gallery = data1[0].gallery_name;
+          });
+        }
+
+        $http.post('/api/data/searchgallery', { museum: museumName }).success(function (data2) {
+          console.log(data2);
+        });
+
+      });
 
       $scope.showForm = false;
       $scope.showResult = true;
+
     };
 
+
+    $scope.resetForm = function() {
+      instantiate();
+
+      $scope.showResult = false;
+      $scope.showForm = true;
+    }
   });
