a78a65d7506986c8e634d623e8a44ce5ec9215ad
Gary Lin <garylinrule@gmail.com>
2015-04-08 21:22:37 -0400

    EYS

--- server/api/data/data.controller.js
+++ server/api/data/data.controller.js
@@ -89,7 +89,7 @@ exports.searchrestaurantcuisine = function (req, res) {
 };
 
 exports.searchfoodtruckcuisine = function (req, res) {
-    var query = "SELECT area_name, truck_name, cuisine, trajectory FROM neighborhood N, area_food_truck F WHERE N.nid = F.nid; AND area_name = '" + req.body.neighborhood.area_name + "' and cuisine = '" + req.body.cuisine.cuisine + "'";
+    var query = "SELECT area_name, truck_name, cuisine, trajectory FROM neighborhood N, area_food_truck F WHERE N.nid = F.nid AND area_name = '" + req.body.neighborhood.area_name + "' and cuisine = '" + req.body.cuisine.cuisine + "'";
     pool.query(query, function(err, rows, field) {
     	if (!err) {
     		console.log('Neighborhood requested!');
@@ -108,7 +108,7 @@ exports.searchfoodtruckcuisine = function (req, res) {
 };
 
 exports.searchfoodtruck = function (req, res) {
-    var query = "SELECT area_name, truck_name, cuisine, trajectory FROM neighborhood N, area_food_truck F WHERE N.nid = F.nid; AND area_name = '" + req.body.neighborhood.area_name + "'";
+    var query = "SELECT area_name, truck_name, cuisine, trajectory FROM neighborhood N, area_food_truck F WHERE N.nid = F.nid AND area_name = '" + req.body.neighborhood.area_name + "'";
     pool.query(query, function(err, rows, field) {
     	if (!err) {
     		console.log('Neighborhood requested!');
